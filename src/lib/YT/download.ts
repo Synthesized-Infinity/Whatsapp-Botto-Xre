fcfc83cea7c170a00aa25eddef3accfe322e7176
Alensaito1 <erwinsmithscout1@gmail.com>
2021-04-18 10:34:31 +0530

    Fix YT (2)

--- src/lib/YT/download.ts
+++ src/lib/YT/download.ts
@@ -8,13 +8,13 @@ import responses from '../responses.json'
 export const download = async (url: string, type: 'video' | 'audio'): Promise<IReply | string> => {
     if (!validateURL(url)) return responses['invalid-url'].replace('{W}', 'YT').replace('{U}', url)
     const video = type === 'video'
-    const filename = `${tmpdir()}/${Math.random().toString(30)}.${video ? 'mp4' : 'mp3'}`
+    let filename = `${tmpdir()}/${Math.random().toString(30)}.${video ? 'mp4' : 'mp3'}`
     const { videoDetails: info } = await getInfo(url)
     if (Number(info.lengthSeconds) > 600) return responses['video-duration-clause']
     const stream = createWriteStream(filename)
     ytdl(url, { quality: !video ? 'highestaudio' : 'highest' }).pipe(stream)
-    await new Promise((resolve, reject) => {
-        stream.on('end', resolve)
+    filename = await new Promise((resolve, reject) => {
+        stream.on('finish', () => resolve(filename))
         stream.on('error', (err) => reject(err && console.log(err)))
     })
     const caption = `📗 *Title:* ${info.title}\n📙 *Description:* ${info.description}\n📘 *Author:* ${info.author}`
@@ -27,3 +27,4 @@ export const getYTMediaFromUrl = async (url: string, type: 'video' | 'audio'): P
     if (typeof media === 'string') return { body: media }
     return { ...media, type: type === 'audio' ? MessageType.audio : MessageType.video }
 }
+
