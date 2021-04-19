143e29b3bac4cf82482e19aea9dd9c28e4be3465
Alensaito1 <erwinsmithscout1@gmail.com>
2021-04-19 21:55:22 +0530

    Lyrics Fix

--- src/lib/lyrics.ts
+++ src/lib/lyrics.ts
@@ -3,6 +3,6 @@ import Utils from '../Utils'
 export const lyrics = async (term: string): Promise<string> => {
     if (!process.env.EIF) return responses.warinings.EIF
     if (!term) responses['empty-query']
-    const data = await Utils.fetch(`${process.env.EIF}/lyrics?=${encodeURI(term)}`, {})
+    const data = await Utils.fetch(`${process.env.EIF}/lyrics?term=${encodeURI(term)}`, {})
     return (data.status !== 200) ? data.error : responses.lyrics.replace(`{T}`, Utils.capitalize(data.term)).replace(`{L}`, data.lyrics)
 }
