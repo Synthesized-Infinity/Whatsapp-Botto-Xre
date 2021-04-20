04748527d705a2aa3496c55584a15a0201f19f48
Alensaito1 <erwinsmithscout1@gmail.com>
2021-04-20 10:25:24 +0530

    Wallpaper fix

--- src/lib/wallpaper.ts
+++ src/lib/wallpaper.ts
@@ -5,24 +5,26 @@ import Utils from '../Utils'
 import responses from './responses.json'
 const wallClient = new AnimeWallpaper()
 
-const alphacoders = async (term: string): Promise<Buffer| string> => {
+const alphacoders = async (term: string): Promise<Buffer| null> => {
     try {
         return Utils.download((await wallClient.getAnimeWall1({ search: term, page: 1 }))[0].image)
     } catch(err) {
-        return responses['no-search-results'].replace('{T}', term)
+        return null
     }
 }
 
-const wallpapercave = async (term: string): Promise<Buffer| string> => {
+const wallpapercave = async (term: string): Promise<Buffer| null> => {
     try {
         return Utils.download((await wallClient.getAnimeWall2(term))[0].image)
     } catch(err) {
-        return responses['no-search-results'].replace('{T}', term)
+        return null
     }
 }
 export const wallpaper = async (term: string): Promise<IReply> => {
     if (!term) return { body: responses['wrong-format'] }
     const alpha = await alphacoders(term)
-    if (Buffer.isBuffer(alpha)) return { body: alpha, type: MessageType.image }
-    return { body: await wallpapercave(term), type: MessageType.image}
+    if (alpha) return { body: alpha, type: MessageType.image }
+    const cave = await wallpapercave(term)
+    if (cave) return { body: cave, type: MessageType.image}
+    return { body: responses['no-search-results'].replace('{T}', term)}
 }
