2f7ed6a6c6a503bd10610a47f50671d744eb2ec7
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-04-22 16:16:33 +0530

    subred fix (2)

--- src/lib/reddit.ts
+++ src/lib/reddit.ts
@@ -1,3 +1,4 @@
+import { MessageType } from '@adiwajshing/baileys'
 import { readFile } from 'fs-extra'
 import { join } from 'path'
 import { IReply } from '../Typings'
@@ -7,14 +8,15 @@ import responses from './responses.json'
 export const reddit = async (subreddit: string, safe: boolean): Promise<IReply> => {
     if (!subreddit) return { body: responses['wrong-format'] }
     try {
-        const post = await Utils.fetch(`https://meme-api.herokuapp.com/gimme/${encodeURI(subreddit)}`, {})
+        const post = await Utils.fetch(`https://meme-api.herokuapp.com/gimme/${encodeURI(subreddit.trim())}`, {})
         if (post.nsfw && safe)
-            return { body: await readFile(join(__dirname, '..', '..', 'assets', '18+.jpg')), caption: `` }
+            return { body: await readFile(join(__dirname, '..', '..', 'assets', '18+.jpg')), caption: responses.mod['no-nsfw'], type: MessageType.image }
         return {
             body: await Utils.download(post.url),
-            caption: `📗*Title:* ${post.title}\n📘*Author:* ${post.author}\n📙*Post:* ${post.postLink}`
+            caption: `📗*Title:* ${post.title}\n📘*Author:* ${post.author}\n📙*Post:* ${post.postLink}`,
+            type: MessageType.image
         }
     } catch (err) {
-        return { body: err.response.data.error }
+        return { body: `🎯 *The given subreddit possibly is Invalid or this subreddit does not have any posts containing images*` }
     }
 }
