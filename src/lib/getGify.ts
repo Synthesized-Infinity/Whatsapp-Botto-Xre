77701c44867507f356b8b9b884e02207ffed8147
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-05-25 15:49:33 +0530

    `getGify` sidenote intellisense

--- src/lib/getGify.ts
+++ src/lib/getGify.ts
@@ -3,6 +3,13 @@ import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import { IReply } from '../Typings'
 import Utils from '../Utils' // Importing Utils to get JSON fetch function
 // Main function: returns a Promise of type string
+
+
+/** Side-note
+* To get gif in other formats, I'd recommed you exploring the json itself which provided link returns.
+* This stability of the url and API_KEY is not guaranteed
+* Regards ~ Somnath Das 
+*/
 const getGify = async (keyword: string): Promise<string | null> => {
     // Fetching gif json by providing keyword
     const data: { results: IGifyResponse[] } = await Utils.fetch(
@@ -12,10 +19,6 @@ const getGify = async (keyword: string): Promise<string | null> => {
     return data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.mp4?.url
 }
 
-// Side-note
-// To get gif in other formats, I'd recommed you exploring the json itself which provided link returns.
-// This stability of the url and API_KEY is not guaranteed
-// Regards ~ Somnath Das
 
 export const getGifReply = async (query: string, users?: [string, string]): Promise<IReply> => {
     if (!query) return { body: `Please Provide the query to search for!` }
