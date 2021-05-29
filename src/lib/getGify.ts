2ec8337b6538f64d88387790dd4a5838f7762030
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-05-29 22:01:12 +0530

    Format using `prettier`

--- src/lib/getGify.ts
+++ src/lib/getGify.ts
@@ -1,12 +1,12 @@
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import { IReply } from '../Typings'
-import Utils from '../Utils' 
+import Utils from '../Utils'
 
 /** Side-note
-* To get gif in other formats, I'd recommed you exploring the json itself which provided link returns.
-* This stability of the url and API_KEY is not guaranteed
-* Regards ~ Somnath Das 
-*/
+ * To get gif in other formats, I'd recommed you exploring the json itself which provided link returns.
+ * This stability of the url and API_KEY is not guaranteed
+ * Regards ~ Somnath Das
+ */
 const getGify = async (keyword: string): Promise<string | null> => {
     // Fetching gif json by providing keyword
     const data: { results: IGifyResponse[] } = await Utils.fetch(
@@ -15,7 +15,6 @@ const getGify = async (keyword: string): Promise<string | null> => {
     )
     return data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.mp4?.url
 }
-
 
 export const getGifReply = async (query: string, users?: [string, string]): Promise<IReply> => {
     if (!query) return { body: `Please Provide the query to search for!` }
